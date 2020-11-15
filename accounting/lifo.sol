c042aba172ff82ac20fdd92ccbb71d6478b16c30
xujiahuayz <31636867+xujiahuayz@users.noreply.github.com>
2020-11-15 20:41:30 +0000

    Update lifo.sol

--- accounting/lifo.sol
+++ accounting/lifo.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
 
+
+contract LIFO {
+  // initialize inventory balance with an empty array
+  int[] inventory;
+  int[] profits;
+  
+    function getInventory() public view returns(int[] memory) {
+        return inventory;
+    }
+    
+    function getProfits() public view returns(int[] memory) {
+        return profits;
+    }
+    
+    function addInventory(int inventoryAdded) public {
+        inventory.push(inventoryAdded);
+    }
+
+    function sellInventory(int salePrice) public {
+        // calculate profit of this sale
+        int profit = salePrice - inventory[inventory.length - 1];
+        
+        // remove the last element in the `inventory` array
+        inventory.pop();
+        
+        // add an element into the `profits` array
+        profits.push(profit);
+    }
+}
